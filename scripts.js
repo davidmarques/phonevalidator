9407dc82d1acf175f9a90b25e5d55d947f0c2e84
davidmarques <davidmarquesbv@gmail.com>
2024-03-15 14:53:08 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -387,7 +387,7 @@ function selectOption() {
 }
 
 function cleanInput(){
-  var phonefield = document.querySelector(".phvCpnum input");
+  var phonefield = document.querySelectorAll(".phvCpnum input");
   console.log(phonefield);
   for (startInput of phonefield) {
     console.log(phonefield);
